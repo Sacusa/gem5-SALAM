c0c9d6805a9715bc857d0d2097101f0e9012fb58
Sudhanshu Gupta <sudhanshugupta96@gmail.com>
2023-12-22 17:44:46 -0500

    Replace execution time by accelerator occupancy

--- BM_ARM_OUT/scripts/comb_3/plot_accelerator_occupancy.py
+++ BM_ARM_OUT/scripts/comb_3/plot_accelerator_occupancy.py
@@ -22,10 +22,10 @@ def geo_mean(iterable):
     return a.prod()**(1.0/len(a))
 
 def add_plot(offset, policy, label):
-    plt.bar([i+offset for i in x], stat_values[policy],
+    plt.bar([i+offset for i in x], occupancy[policy],
             edgecolor=edgecolors[policy], width=width, label=label,
             fc=colors[policy], hatch=hatch[policy], zorder=1)
-    plt.bar([i+offset for i in x], stat_values[policy], fc='none',
+    plt.bar([i+offset for i in x], occupancy[policy], fc='none',
             edgecolor='k', width=width, zorder=2)
 
 applications = ['canny', 'deblur', 'gru', 'harris', 'lstm']
@@ -33,29 +33,30 @@ policies = ['FCFS', 'GEDF_D', 'GEDF_N', 'LAX', 'HetSched', 'ELF']
 
 app_mixes = sorted([c for c in itertools.combinations(applications, 3)])
 
-exec_time = {p:[] for p in policies}
+occupancy = {p:[] for p in policies}
 
-for app_mix in app_mixes:
-    app_mix_str = ''
-    for app in applications:
-        app_mix_str += app + '_'
-        if app in app_mix: app_mix_str += '1_'
-        else:              app_mix_str += '0_'
+for policy in policies:
+    for app_mix in app_mixes:
+        occupancy[policy].append(0)
 
-    for policy in policies:
+        app_mix_str = ''
+        for app in applications:
+            app_mix_str += app + '_'
+            if app in app_mix: app_mix_str += '1_'
+            else:              app_mix_str += '0_'
+
+        for line in open('../../comb_3/' + app_mix_str + policy + \
+                '/debug-trace.txt'):
+            if 'Runtime' in line and 'us' in line:
+                occupancy[policy][-1] += float(line.split()[1])
+
         for line in open('../../comb_3/' + app_mix_str + policy + \
                 '/stats.txt'):
             if 'sim_ticks' in line:
-                exec_time[policy].append(int(line.split()[1]))
+                occupancy[policy][-1] /= float(line.split()[1]) / 1000_000
+                break
 
-    # normalize the values
-    norm_value = exec_time['LAX'][-1]
-    for policy in policies:
-        exec_time[policy][-1] /= norm_value
-
-# calculate geomean
-for policy in policies:
-    exec_time[policy].append(geo_mean(exec_time[policy]))
+    occupancy[policy].append(geo_mean(occupancy[policy]))
 
 x = [i for i in range(len(app_mixes) + 1)]
 x_labels = ["".join([a[0].upper() for a in app_mix])
@@ -64,25 +65,23 @@ x_labels = ["".join([a[0].upper() for a in app_mix])
 plt.figure(figsize=(24, 8), dpi=600)
 plt.rc('axes', axisbelow=True)
 
-plot_policies = ['FCFS', 'GEDF_D', 'GEDF_N', 'HetSched', 'ELF']
-width = 0.8 / len(plot_policies)
-if len(plot_policies) % 2 == 0:
-    offset = -width * (0.5 + ((len(plot_policies) / 2) - 1))
+width = 0.8 / len(policies)
+if len(policies) % 2 == 0:
+    offset = -width * (0.5 + ((len(policies) / 2) - 1))
 else:
-    offset = -width * ((len(plot_policies) - 1) / 2)
-for policy in plot_policies:
+    offset = -width * ((len(policies) - 1) / 2)
+for policy in policies:
     plabel = label[policy] if policy in label else policy
     add_plot(offset, policy, plabel)
     offset += width
 
 plt.xticks(x, x_labels, fontsize=30)
 
-plt.ylabel('Execution time (norm. to LAX)', fontsize=30)
+plt.ylabel('Accelerator occupancy', fontsize=30)
 plt.yticks(fontsize=30)
-plt.ylim([0, 1.8])
+plt.ylim([0, 1.7])
 plt.gca().yaxis.set_major_locator(plt.MultipleLocator(0.2))
 
-plt.legend(loc="upper left", ncol=len(policies), fontsize=25)
-plt.grid(color='silver', linestyle='-', linewidth=1)
-plt.savefig('../plots/comb_3/execution_time.pdf',
-        bbox_inches='tight')
+plt.legend(loc="upper left", ncol=6, fontsize=25)
+plt.grid(axis='y', color='silver', linestyle='-', linewidth=1)
+plt.savefig('../plots/comb_3/accelerator_occupancy.pdf', bbox_inches='tight')
